c632afe3656e23fdf14c6393a6be0b8e091f1f0d
Koustubh <koustubhsadavarte378@gmail.com>
2026-01-20 20:25:50 +0530

    Authentication for staff and staff controller

--- ComputerSeekho/src/main/java/com/example/demo/ComputerSeekhoApplication.java
+++ ComputerSeekho/src/main/java/com/example/demo/ComputerSeekhoApplication.java
@@ -3,13 +3,16 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("com.example.entity")
+@ComponentScan(basePackages = "com.example")
+@EnableJpaRepositories(basePackages = "com.example.repository")
+@EntityScan(basePackages = "com.example.entity")
 public class ComputerSeekhoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ComputerSeekhoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerSeekhoApplication.class, args);
+    }
 }
